800f63f2595f7ea95f88ee52d294ebec989401b2
Subash Singh Dhami <subashdhamee@gmail.com>
2026-09-18 14:05:06 +0545

    feat: configure OAuth2 authorization server

--- src/main/java/io/github/mesubash/springbootoauth2authorizationserver/config/AuthorizationServerConfig.java
+++ src/main/java/io/github/mesubash/springbootoauth2authorizationserver/config/AuthorizationServerConfig.java
@@ -1,5 +1,10 @@
 package io.github.mesubash.springbootoauth2authorizationserver.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -7,22 +12,30 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
 
 @Configuration(proxyBeanMethods = false)
@@ -74,7 +87,7 @@ public class AuthorizationServerConfig {
 
 
     // temp user
-    // Later : UserDetailsService -> database-backed authentication
+    // Later: UserDetailsService -> database-backed authentication
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails user = User.builder()
@@ -110,7 +123,7 @@ public class AuthorizationServerConfig {
                         "http://127.0.0.1:8081/callback"
                 )
                 .postLogoutRedirectUri(
-                        "http::127.0.0.1:8081"
+                        "http://127.0.0.1:8081"
                 )
 
                 .scope(OidcScopes.OPENID)
@@ -126,6 +139,67 @@ public class AuthorizationServerConfig {
 
                 .build();
         return new InMemoryRegisteredClientRepository(client);
+    }
+
+    // dev RSA signing key
+    // a new key pair generated every application restart
+
+    //later: we will replace this with persistent key management
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    private static KeyPair generateRsaKey() {
+
+        try {
+            KeyPairGenerator generator =
+                    KeyPairGenerator.getInstance("RSA");
+
+            generator.initialize(2048);
+
+            return generator.generateKeyPair();
+
+        } catch (Exception exception) {
+            throw new IllegalStateException(
+                    "Failed to generate RSA key pair",
+                    exception
+            );
+        }
+    }
+
+    // required for OIDC endpoints such as UserInfo
+    @Bean
+    public JwtDecoder jwtDecoder(
+            JWKSource<SecurityContext> jwkSource
+    ) {
+
+        return OAuth2AuthorizationServerConfiguration
+                .jwtDecoder(jwkSource);
+    }
+
+
+    //config of this authorization server itself
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 
