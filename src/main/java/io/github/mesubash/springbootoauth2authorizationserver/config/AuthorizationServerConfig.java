afbfcf30fd0b8c6ce265ec515bd3d52252e7a4a9
Subash Singh Dhami <subashdhamee@gmail.com>
2026-09-18 13:33:46 +0545

    add temp client  and user

--- src/main/java/io/github/mesubash/springbootoauth2authorizationserver/config/AuthorizationServerConfig.java
+++ src/main/java/io/github/mesubash/springbootoauth2authorizationserver/config/AuthorizationServerConfig.java
@@ -7,9 +7,23 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+import java.util.UUID;
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
@@ -57,4 +71,62 @@ public class AuthorizationServerConfig {
 
         return http.build();
     }
+
+
+    // temp user
+    // Later : UserDetailsService -> database-backed authentication
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    //temp OauthClient
+    //Later:
+    //RegisteredClientRepository -> PostgreSQL.
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
+        RegisteredClient client = RegisteredClient
+                .withId(UUID.randomUUID().toString())
+                .clientId("demo-client")
+                .clientSecret(passwordEncoder.encode("demo-secret"))
+                .clientAuthenticationMethod(
+                        ClientAuthenticationMethod.CLIENT_SECRET_BASIC
+                )
+                .authorizationGrantType(
+                        AuthorizationGrantType.AUTHORIZATION_CODE
+                )
+                .authorizationGrantType(
+                        AuthorizationGrantType.REFRESH_TOKEN
+                )
+
+                .redirectUri(
+                        "http://127.0.0.1:8081/callback"
+                )
+                .postLogoutRedirectUri(
+                        "http::127.0.0.1:8081"
+                )
+
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("read")
+                .scope("write")
+                .clientSettings(
+                        ClientSettings.builder()
+                                .requireProofKey(true)
+                                .requireAuthorizationConsent(true)
+                                .build()
+                )
+
+                .build();
+        return new InMemoryRegisteredClientRepository(client);
+    }
+
+
 }
